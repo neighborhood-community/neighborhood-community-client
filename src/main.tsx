cf352e44eda072076a35c424e735ae98e0db6fe8
seonjae <tjswo2292@naver.com>
2023-07-13 21:01:35 +0900

    init setting

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import App from "./App.tsx";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<QueryClientProvider client={queryClient}>
+		<ReactQueryDevtools initialIsOpen={true} />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</QueryClientProvider>
+);
