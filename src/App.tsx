fb137f11734df90b300273cb5758f457a1a40c8b
seonjae <tjswo2292@naver.com>
2023-07-19 17:33:05 +0900

    feat: 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './components/home';
+import Posts from './components/posts';
+
 function App() {
-  return <>Init setting</>;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/post',
+          element: <Posts />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
