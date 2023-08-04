5b03f9d384eab66b0639cf92c63fa1330941acce
seonjae <tjswo2292@naver.com>
2023-08-04 15:13:46 +0900

    feat: ProtectedRouter 추가

---
+++ src/router/Router.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Layout from '../components/layout/Layout';
+import Home from '../components/home';
+import Posts from '../components/posts';
+import Verify from '../components/verify/Verify';
+import MyPage from '../components/mypage';
+
+import { ROUTES } from './routes';
+import ProtectedRouter from './ProtectedRouter';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path={ROUTES.MAIN} element={<Home />} />
+          <Route element={<ProtectedRouter />}>
+            <Route path={ROUTES.POST} element={<Posts />} />
+            <Route path={ROUTES.MYPAGE} element={<MyPage />} />
+          </Route>
+        </Route>
+
+        <Route path={ROUTES.VERIFY} element={<Verify />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
