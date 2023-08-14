84c6fe69bf1e074499fdaedd5f5f356442b424d1
seonjae <tjswo2292@naver.com>
2023-08-14 23:05:23 +0900

    feat: mypage pagination 추가

--- src/components/mypage/organism/UserPostListSection.tsx
+++ src/components/mypage/organism/UserPostListSection.tsx
@@ -1,24 +1,45 @@
-import usePostList from '../hook/usePostList';
-
-import { numberList } from '../../../utils/numberList';
-
 import { styled } from 'styled-components';
-import useClickedPage from '../hook/useClickedPage';
+import { BiSolidLeftArrow, BiSolidRightArrow } from 'react-icons/bi';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+
 import PostContents from '../molecules/PostContents';
+import PageNumberButton from '../molecules/PageNumberButton';
+
+import usePostList from '../hook/usePostList';
+import useClickedPage from '../hook/useClickedPage';
+import { numberList } from '../../../utils/numberList';
+import usePostListSize from '../hook/usePostListSize';
+import { StyledProps } from './type';
+import { useEffect, useState } from 'react';
 
 const UserPostListSection = () => {
-  const { pageNumber, handlePageNumber } = useClickedPage();
-  const { postList, postListSize } = usePostList(pageNumber);
+  const [translateValue, setTranslateValue] = useState(0);
+
+  const { postListSize } = usePostListSize();
+  const { pageNumber, handlePageNumber } = useClickedPage(postListSize);
+  const { postList } = usePostList(pageNumber);
 
   const postNumberList = numberList(postListSize);
-
-  const handleMovePage = (clickedNumber: number) => {
-    handlePageNumber(clickedNumber);
-  };
 
   const handleEnd = (endNumber: number) => {
     handlePageNumber(endNumber);
   };
+
+  const pageMovePrevSingly = () => {
+    handlePageNumber(pageNumber - 1);
+  };
+
+  const pageMoveNextSingly = () => {
+    handlePageNumber(pageNumber + 1);
+  };
+
+  useEffect(() => {
+    if (pageNumber === 1) {
+      setTranslateValue(0);
+    } else {
+      setTranslateValue(pageNumber % 5 === 0 ? Math.floor(pageNumber / 5) - 1 : Math.floor(pageNumber / 5));
+    }
+  }, [pageNumber]);
 
   return (
     <Box>
@@ -28,15 +49,31 @@ const UserPostListSection = () => {
             <PostContents key={id} title={title} date={date} />
           ))}
         </ContentsListBox>
-        <PageNumberListWrapper>
-          <Prev onClick={() => handleEnd(1)}>⬅️</Prev>
-          {postNumberList.map(element => (
-            <PageNumberBtn onClick={() => handleMovePage(element)} key={element}>
-              {element}
-            </PageNumberBtn>
-          ))}
-          <Next onClick={() => handleEnd(postListSize)}>➡️</Next>
-        </PageNumberListWrapper>
+        <PageNumberListBox>
+          <PageMoveButtonWrapper>
+            <FirstMove onClick={() => handleEnd(1)}>
+              <FaChevronLeft />
+            </FirstMove>
+            <PrevSingly onClick={pageMovePrevSingly}>
+              <BiSolidLeftArrow />
+            </PrevSingly>
+          </PageMoveButtonWrapper>
+          <PostNumberListBox>
+            <PostNumberListWrapper $translateValue={translateValue}>
+              {postNumberList.map(element => (
+                <PageNumberButton isChecked={element === pageNumber ? true : false} key={element} pageNumberData={element} handlePageNumber={handlePageNumber} />
+              ))}
+            </PostNumberListWrapper>
+          </PostNumberListBox>
+          <PageMoveButtonWrapper>
+            <NextSingly onClick={pageMoveNextSingly}>
+              <BiSolidRightArrow />
+            </NextSingly>
+            <LastMove onClick={() => handleEnd(postListSize)}>
+              <FaChevronRight />
+            </LastMove>
+          </PageMoveButtonWrapper>
+        </PageNumberListBox>
       </PagiNationBox>
     </Box>
   );
@@ -59,21 +96,27 @@ const PagiNationBox = styled.div`
 `;
 const ContentsListBox = styled.div`
   flex: 1;
-  /* overflow-y: hidden; */
+  overflow-y: hidden;
 `;
-const Prev = styled.button``;
-const Next = styled.button``;
-const PageNumberListWrapper = styled.div`
+const PageMoveButtonWrapper = styled.div``;
+const PrevSingly = styled.button``;
+const NextSingly = styled.button``;
+const FirstMove = styled.button``;
+const LastMove = styled.button``;
+const PageNumberListBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: inherit;
 `;
-const PageNumberBtn = styled.button`
-  width: 2rem;
-  height: 2rem;
-  border: 0.1rem solid #e1e2e3;
-  border-radius: 0.3rem;
+const PostNumberListBox = styled.div`
+  overflow-x: hidden;
+  width: 11.35rem;
+  height: 2.2rem;
+`;
+const PostNumberListWrapper = styled.div<StyledProps>`
+  display: flex;
+  transform: translateX(${({ $translateValue }) => `${-1 * 114 * $translateValue}px`});
 `;
 
 export default UserPostListSection;
