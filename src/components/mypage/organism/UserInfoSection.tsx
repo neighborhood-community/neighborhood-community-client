a2733c55f558d9414bb81f495600dad8befad37d
seonjae <tjswo2292@naver.com>
2023-08-14 23:04:59 +0900

    feat: mypage user card

--- src/components/mypage/organism/UserInfoSection.tsx
+++ src/components/mypage/organism/UserInfoSection.tsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
+
 import UserInfoCard from '../molecules/UserInfoCard';
 
+import { fetchUserInfo } from '../../../apis/user';
+
 const UserInfoSection = () => {
+  const [userInfo, setUserInfo] = useState({
+    gender: '',
+    nickname: '',
+    profile_img: '',
+  });
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      try {
+        const response = await fetchUserInfo();
+        const { gender, nickname, profile_img } = response.data.data;
+
+        setUserInfo({
+          gender,
+          nickname,
+          profile_img,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getUserInfo();
+  }, []);
+
   return (
     <Box>
-      <UserInfoCard />
+      <UserInfoCard userGender={userInfo.gender} userNickname={userInfo.nickname} userProfileImgPath={userInfo.profile_img} />
     </Box>
   );
 };
