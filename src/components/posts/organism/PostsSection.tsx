a2308133362cb0ccfc83441071fd176895e357e6
seonjae <tjswo2292@naver.com>
2023-08-20 15:33:38 +0900

    feaet: post 페이지 추가

--- src/components/posts/organism/PostsSection.tsx
+++ src/components/posts/organism/PostsSection.tsx
@@ -1,21 +1,79 @@
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { styled } from 'styled-components';
-import TextInput from '../../common/TextInput';
+
+import { publicApi } from '../../../apis/core/axios';
+import { PATH } from '../../../apis/core/constants';
 import ContentDetail from '../molecules/ContentDetail';
+import SubmitButton from '../molecules/SubmitButton';
+import ActivitySelect from '../molecules/ActivitySelect';
+import TextInput from '../../common/TextInput';
+
+type ContentsType = {
+  contentsTitle: string;
+  contentsRegion: string;
+  contentsCategory: string;
+  contentsDetail: string;
+};
 
 const PostsSection = () => {
+  const navigate = useNavigate();
+  const {
+    handleSubmit,
+    register,
+    setValue,
+    formState: { isValid },
+  } = useForm<ContentsType>({
+    mode: 'onSubmit',
+    defaultValues: {
+      contentsTitle: '',
+      contentsRegion: '',
+      contentsCategory: '카테고리',
+      contentsDetail: '',
+    },
+  });
+
+  const handleOnSubmit = async (data: ContentsType) => {
+    const { contentsTitle, contentsRegion, contentsCategory, contentsDetail } = data;
+
+    try {
+      const response = await publicApi.POST(`${PATH.POST_CONTENTS}`, {
+        title: contentsTitle,
+        region: contentsRegion,
+        category: contentsCategory,
+        content: contentsDetail,
+      });
+
+      navigate('/');
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { target } = e;
+    const { value } = target;
+
+    setValue('contentsCategory', value);
+  };
+
   return (
     <Box>
-      <Content>
-        <TextInput placeHolder="제목을 입력해주세요" />
-        <TextInput placeHolder="지역을 입력해주세요" />
-        <ContentDetail />
-      </Content>
+      <ContentForm onSubmit={handleSubmit(handleOnSubmit)}>
+        <TextInput register={register('contentsTitle', { required: true })} placeHolder="제목을 입력해주세요" />
+        <TextInput register={register('contentsRegion', { required: { value: true, message: '지역을 입력해주세요' } })} placeHolder="지역을 입력해주세요" />
+        <ActivitySelect register={register('contentsCategory', { required: true })} handleSelectChange={handleSelectChange} />
+        <ContentDetail register={register('contentsDetail', { required: { value: true, message: '내용을 입력해주세요' } })} />
+        <SubmitButton isValid={isValid} />
+      </ContentForm>
     </Box>
   );
 };
 
 const Box = styled.div``;
-const Content = styled.div`
+const ContentForm = styled.form`
   width: 700px;
   margin: 0 auto;
   margin-top: 20px;
