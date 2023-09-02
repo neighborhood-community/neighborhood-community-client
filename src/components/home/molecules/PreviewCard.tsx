ce37ac3afcefdbd13dd0f99d0ce445903b2dc9e9
seonjae <tjswo2292@naver.com>
2023-09-02 14:06:58 +0900

    feat&design: PreviewCard 컴포넌트 추가

--- src/components/home/molecules/PreviewCard.tsx
+++ src/components/home/molecules/PreviewCard.tsx
@@ -1,28 +1,51 @@
 import { Link } from 'react-router-dom';
 import { M } from './style';
 
-import Profile from '/assets/image/profile-image.svg';
+interface PreviewCard {
+  id: string;
+  region: string;
+  content: string;
+  category: string;
+  profileImg: string;
+  gender: string;
+  date: string;
+}
 
-const PreviewCard = () => {
+const PreviewCard = ({ id, region, content, category, profileImg, gender, date }: PreviewCard) => {
   return (
-    <M.PreviewCardBox>
-      <Link to="/">
+    <Link
+      to={{
+        pathname: '/contents',
+        search: `userId=${id}`,
+      }}
+    >
+      <M.PreviewCardBox>
         <M.CardDetailBox>
           <M.CardImageWrapper>
             <M.ImageWrapper>
-              <M.Image src={Profile} alt="프로필이미지" />
+              <M.Image src={profileImg} alt="프로필이미지" />
             </M.ImageWrapper>
           </M.CardImageWrapper>
           <M.CardDescBox>
             <M.UserInformationBox>
-              <M.UserGender>남자</M.UserGender>
-              <M.UserLocatoin>안산</M.UserLocatoin>
+              <M.UserGender>
+                <M.Gender>{gender && '비공개'}</M.Gender>
+              </M.UserGender>
+              <M.UserRegion>
+                <M.Region>{region}</M.Region>
+              </M.UserRegion>
+              <M.ContentsCategory>
+                <M.Category>{category}</M.Category>
+              </M.ContentsCategory>
+              <M.ContentsDate>
+                <M.Date>{date}</M.Date>
+              </M.ContentsDate>
             </M.UserInformationBox>
-            <M.BoardTitle>안녕하세요! 새벽에 수산물을 받아서 신선하게 배송해드리고 있습니다.</M.BoardTitle>
+            <M.BoardTitle>{content}</M.BoardTitle>
           </M.CardDescBox>
         </M.CardDetailBox>
-      </Link>
-    </M.PreviewCardBox>
+      </M.PreviewCardBox>
+    </Link>
   );
 };
 
