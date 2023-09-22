7cec33741da14551b670e19f0960dee8627d4917
seonjae <tjswo2292@naver.com>
2023-09-22 15:19:44 +0900

    chore: refresh token 검증 path 추가

--- src/apis/core/constants.ts
+++ src/apis/core/constants.ts
@@ -1,6 +1,7 @@
 export const PATH = {
   AUTH_KAKAO: '/kakao',
   AUTH_NEWTOKEN: '/newtoken',
+  AUTH_REFRESH_TOKEN_VALIDITY: '/refresh-token-validity',
   MYPAGE: '/mypage',
   POST: '/post',
   POST_CONTENTS: '/post/i',
