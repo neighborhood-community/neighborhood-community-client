829207905fae4e9a7d781f4ef0421b657ea4e9a4
seonjae <tjswo2292@naver.com>
2023-09-22 15:12:37 +0900

    feat: 로그인 유무 전역 state context api 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import ValidProvider from './context/authorizationContext';
 import Router from './router/Router';
-
 import { GlobalStyle } from './style/globalStyle';
 
 function App() {
   return (
-    <>
+    <ValidProvider>
       <Router />
       <GlobalStyle />
-    </>
+    </ValidProvider>
   );
 }
 
