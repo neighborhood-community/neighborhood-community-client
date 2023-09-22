b29084797d12f34691855fda49e3d64eefefd2ac
seonjae <tjswo2292@naver.com>
2023-09-22 15:21:25 +0900

    feat: refresh token 검증 api 호출 추가

--- src/router/ProtectedRouter.tsx
+++ src/router/ProtectedRouter.tsx
@@ -1,10 +1,37 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { accessTokenManage } from '../utils/storage';
+import { useContext, useEffect } from 'react';
+
+import { Outlet } from 'react-router-dom';
+
+import AuthenticationError from './AuthenticationError';
+import { ValidContext } from '../context/authorizationContext';
+import { PATH } from '../apis/core/constants';
+import { authApi } from '../apis/core/axios';
+import { accessTokenManage, refreshTokenManage } from '../utils/storage';
 
 const ProtectedRouter = () => {
-  const token = accessTokenManage.GET_TOKEN();
+  const refreshToken = refreshTokenManage.GET_COOKIE();
 
-  return token ? <Outlet /> : <Navigate to={'/'} />;
+  const { isValid, expiredRefreshToken, unexpiredRefreshToken } = useContext(ValidContext);
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await authApi.GET(`${PATH.AUTH_REFRESH_TOKEN_VALIDITY}`, refreshToken);
+
+        unexpiredRefreshToken();
+
+        return response;
+      } catch (error) {
+        accessTokenManage.DELETE_TOKEN();
+        refreshTokenManage.DELETE_COOKIE();
+        expiredRefreshToken();
+      }
+    };
+
+    verifyToken();
+  }, []);
+
+  return isValid ? <Outlet /> : <AuthenticationError />;
 };
 
 export default ProtectedRouter;
